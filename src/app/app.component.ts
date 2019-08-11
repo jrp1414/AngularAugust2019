1cc1a4522308bcf09062f2b0693606c065948556
Ranjan <jyotiranjan.p14@gmail.com>
2019-08-11 12:10:30 +0530

    Angular Class Started

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // selector: 'app-root',
+  // selector: '[app-root]',
+  selector: '.app-root',
+  // templateUrl: './app.component.html',
+  // template:"<app-header></app-header><h1>Inline Template Example</h1>",
+  // template:`
+  // <app-header></app-header>
+  // <h1>Inline Template Example</h1>`,
+  templateUrl:"./app.component.html"  
 })
 export class AppComponent {
-  title = 'WeekendAugust';
+  title = 'Weekend August';
 }
